e97a0d342f6389e14ae8d2788f074ff97df0a099
vanessaep <perezvanessae@gmail.com>
2026-04-22 19:42:07 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Objective 
+# Analyze retail sales data to uncover trends, identify top-performing categories, and generate business insights. 
+# Dataset
+# Source: Kaggle (Superstore Sales Dataset)
+# Key Questions: 
+# Which product categories generate the most revenue?
+# Which regions perform best?
+# How do sales change over time?
+
+import pandas as pd
+# Load dataset
+df = pd.read_csv("data/superstore.csv", encoding='latin1')
+
+# Convert dates 
+df['Order Date']
